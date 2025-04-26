39d4e9dd4814da605ca320370dd2ef2415643610
MMazenA <jazenjirza@gmail.com>
2025-04-26 18:51:16 -0400

    working postgres connection

--- quarry/src/bootstrap.cpp
+++ quarry/src/bootstrap.cpp
@@ -2,6 +2,8 @@
 
 #include <string>
 #include <iostream>
+#include <pqxx/pqxx>
+
 int main(){
     const char *schema_migrations = "CREATE TABLE IF NOT EXISTS schema_migrations ("
                                     "id SERIAL PRIMARY KEY,"
@@ -10,6 +12,23 @@ int main(){
 
 
     std::cout << schema_migrations << std::endl;
+
+    try {
+        pqxx::connection c("host=localhost port=5432 dbname=postgres user=user password=password");
+
+        pqxx::work txn(c);
+
+        pqxx::result r = txn.exec("SELECT version()");
+
+        for (auto row : r) {
+            std::cout << row[0].c_str() << std::endl;
+        }
+
+        txn.commit();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }

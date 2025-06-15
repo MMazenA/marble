0a60253c7cb4d39952e53749f083903c5f662d69
MMazenA <jazenjirza@gmail.com>
2025-06-15 16:44:04 -0400

    cleaning

--- quarry/tests/test_http_client.cpp
+++ quarry/tests/test_http_client.cpp
@@ -2,14 +2,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 
-// TEST_CASE("HTTP Client can connect") {
-//   // MigrationMock migrationMock;
-//   http::response<http::dynamic_body> http_response;
-//   quarry::client("www.example.com", "80", "/", http_response);
-
-//   REQUIRE(static_cast<int>(http_response.result()) == 200);
-// }
-
 TEST_CASE("HTTP Client can connect") {
   quarry::httpClient client("www.example.com", "80");
 

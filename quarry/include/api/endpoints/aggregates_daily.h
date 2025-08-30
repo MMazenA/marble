1f86130211034873964c0c95027b7d2df322c5e9
MMazenA <jazenjirza@gmail.com>
2025-08-30 17:52:30 -0400

    linting

--- quarry/include/api/endpoints/aggregates_daily.h
+++ quarry/include/api/endpoints/aggregates_daily.h
@@ -2,53 +2,54 @@
 #define AGGREGATES_DAILY_H
 
 #include "base_endpoint.h"
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <sstream>
-#include <iostream>
 
 namespace quarry {
 
 namespace Endpoint {
 struct AggregatesDaily : public quarry::BaseEndpoint {
 
-  //Path Params
+  // Path Params
   std::string m_ticker;
   int multiplier = 1;
   timespan_options m_timespan = DAY;
   std::string m_from_date;
   std::string m_to_date;
-  
-  //Query Params
+
+  // Query Params
   bool m_adjusted = true;
   sort_options m_sort = ASC;
   uint16_t m_limit = 120;
 
   /**
-   * \brief Create a request for daily aggregate bars.
+   * @brief Create a request for daily aggregate bars.
    *
-   * \param ticker    Stock symbol (e.g., "AAPL").
-   * \param from_date Start date (inclusive), format `YYYY-MM-DD`.
-   * \param to_date   End date (inclusive), format `YYYY-MM-DD`.
+   * @param ticker    Stock symbol (e.g., "AAPL").
+   * @param from_date Start date (inclusive), format `YYYY-MM-DD`.
+   * @param to_date   End date (inclusive), format `YYYY-MM-DD`.
    *
-   * \see https://polygon.io/docs/rest/stocks/aggregates/custom-bars
-   * 
-   * @todo: this should follow EXACTLY the above docs, and I should rename this class to match it, theres no reason for me to be inventing my own stuff here
+   * @see https://polygon.io/docs/rest/stocks/aggregates/custom-bars
+   *
+   * @todo: this should follow EXACTLY the above docs, and I should rename this
+   * class to match it, theres no reason for me to be inventing my own stuff
+   * here
    */
   AggregatesDaily(std::string ticker, std::string from_date,
                   std::string to_date)
-      : m_ticker(std::move(ticker)){
-        constexpr int validDateSize = 10;
-        if(from_date.size() != validDateSize || to_date.size()!=validDateSize){
-          throw std::invalid_argument("Please configure dates properly");
-        }
-        if(from_date > to_date){
-          throw std::invalid_argument("Start date must be before end date");
-        }
+      : m_ticker(std::move(ticker)) {
+    constexpr int validDateSize = 10;
+    if (from_date.size() != validDateSize || to_date.size() != validDateSize) {
+      throw std::invalid_argument("Please configure dates properly");
+    }
+    if (from_date > to_date) {
+      throw std::invalid_argument("Start date must be before end date");
+    }
 
-        m_from_date = from_date;
-        m_to_date = to_date; 
-        }
+    m_from_date = from_date;
+    m_to_date = to_date;
+  }
 
   std::string method() const override { return "GET"; }
 
@@ -71,19 +72,20 @@ struct AggregatesDaily : public quarry::BaseEndpoint {
         << "&limit=" << m_limit;
     return oss.str();
   }
-  //chainable args
-  AggregatesDaily& setSort(sort_options sort){
+
+  // Query Params
+  AggregatesDaily &setSort(sort_options sort) {
     m_sort = sort;
     return *this;
   }
 
-  AggregatesDaily& setAdjusted(bool adjusted){
+  AggregatesDaily &setAdjusted(bool adjusted) {
     m_adjusted = adjusted;
     return *this;
   }
 
-  AggregatesDaily& setLimit(uint16_t limit){
-    m_limit= limit;
+  AggregatesDaily &setLimit(uint16_t limit) {
+    m_limit = limit;
     return *this;
   }
 };
--- quarry/tests/test_endpoints.cpp
+++ quarry/tests/test_endpoints.cpp
@@ -12,19 +12,23 @@ TEST_CASE("Endpoint can be made") {
 }
 
 TEST_CASE("Endpoint can be customized") {
-auto ep =
+  auto ep =
       quarry::Endpoint::AggregatesDaily("AAPL", "2024-01-09", "2024-01-11");
-      
+
   ep.setAdjusted(false).setLimit(1).setSort(quarry::sort_options::ASC);
   REQUIRE(ep.m_adjusted == false);
-  REQUIRE(ep.m_limit== 1);
-  REQUIRE(ep.m_sort== quarry::sort_options::ASC);
+  REQUIRE(ep.m_limit == 1);
+  REQUIRE(ep.m_sort == quarry::sort_options::ASC);
 }
 
 TEST_CASE("Endpoint throws on improper date") {
-  REQUIRE_THROWS_AS(quarry::Endpoint::AggregatesDaily("AAPL", "2024-1-09", "2024-01-11"), std::invalid_argument);
+  REQUIRE_THROWS_AS(
+      quarry::Endpoint::AggregatesDaily("AAPL", "2024-1-09", "2024-01-11"),
+      std::invalid_argument);
 }
 
 TEST_CASE("Endpoint throws when start date is past end date") {
-  REQUIRE_THROWS_AS(quarry::Endpoint::AggregatesDaily("AAPL", "2030-01-09", "2024-01-11"), std::invalid_argument);
+  REQUIRE_THROWS_AS(
+      quarry::Endpoint::AggregatesDaily("AAPL", "2030-01-09", "2024-01-11"),
+      std::invalid_argument);
 }
